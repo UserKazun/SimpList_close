f1c66a428edb721851ed61565f7b3de8c7b48de9
UserKazun <kazuusjlo8@gmail.com>
2020-10-10 20:49:00 +0900

    add #dataflow データ追加画面の調整

--- SimpList/ViewModel/HomeViewModel.swift
+++ SimpList/ViewModel/HomeViewModel.swift
@@ -18,6 +18,9 @@ class HomeViewModel: ObservableObject {
     // Checking And Updating Date
     let calender = Calendar.current
     
+    // Storing Update Item
+    @Published var updateItem: Task!
+    
     func checkDate() -> String {
         if calender.isDateInToday(date) {
             return "Today"
@@ -39,6 +42,23 @@ class HomeViewModel: ObservableObject {
     }
     
     func writeData(context: NSManagedObjectContext) {
+        // Updating Item
+        if updateItem != nil {
+            // Means Update Old Data
+            updateItem.date = date
+            updateItem.content = content
+            
+            try! context.save()
+            
+            // removing updatingItem if successfull
+            updateItem = nil
+            isNewData.toggle()
+            content = ""
+            date = Date()
+            
+            return
+        }
+        
         let newTask = Task(context: context)
         newTask.date = date
         newTask.content = content
@@ -49,10 +69,21 @@ class HomeViewModel: ObservableObject {
             
             // success means closing view
             isNewData.toggle()
+            content = ""
+            date = Date()
             
         } catch {
             print(error.localizedDescription)
         }
+    }
+    
+    func EditItem(item: Task) {
+        updateItem = item
+        
+        // togging the newDataView
+        date = item.date!
+        content = item.content!
+        isNewData.toggle()
     }
     
 }
